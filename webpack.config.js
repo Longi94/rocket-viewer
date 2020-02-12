1981d0094ada953b6ee909b42bcb95ba0678832e
Long Tran <lngtrn94@gmail.com>
2020-02-12 16:00:33 +0100

    exclude specific files from circular dependency

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const CircularDependencyPlugin = require('circular-dependency-plugin');
 module.exports = {
   plugins: [
     new CircularDependencyPlugin({
-      exclude: /\.wasm|boxcars_wasm\.js|node_modules\\webpack-dev-server\\client\\clients\\SockJSClient\.js/,
+      exclude: /\.wasm|boxcars_wasm\.js|node_modules/,
     })
   ]
 };
