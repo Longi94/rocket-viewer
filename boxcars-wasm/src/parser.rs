88cfbd5d0f90eb220703b235a5d6d8b1e22be18f
Long Tran <lngtrn94@gmail.com>
2020-02-15 00:35:06 +0100

    rust actor parsing

--- boxcars-wasm/src/parser.rs
+++ boxcars-wasm/src/parser.rs
@@ -1,6 +1,7 @@
 use crate::models::{BallFrame, ReplayVersion, FrameData};
-use boxcars::Replay;
+use boxcars::{Replay, ActorId};
 use std::collections::HashMap;
+use crate::actor::{ActorHandler, get_handler};
 
 pub struct FrameParser<'a> {
     pub replay: &'a Replay
@@ -21,15 +22,31 @@ impl<'a> FrameParser<'a> {
         let replay_version = ReplayVersion(
             self.replay.major_version,
             self.replay.minor_version,
-            self.replay.net_version.unwrap_or(0)
+            self.replay.net_version.unwrap_or(0),
         );
 
         let mut frame_data = FrameData::with_capacity(count);
-        let mut actors: HashMap<u32, > = HashMap::new();
+        let mut actors: HashMap<ActorId, dyn ActorHandler> = HashMap::new();
 
         for (i, frame) in frames.frames.iter().enumerate() {
             let ball_frame = BallFrame::new();
             frame_data.ball_frames.push(ball_frame);
+
+            for deleted in frame.deleted_actors {
+                actors.remove(&deleted);
+            }
+
+            for created in frame.new_actors {
+                let object_name = match &self.replay.objects.get(created.object_id.0 as usize) {
+                    None => continue,
+                    Some(&object_name) => object_name
+                };
+
+                let handler = match get_handler(object_name.as_str()) {
+                    None => continue,
+                    Some(b) => b
+                };
+            }
         }
 
         Ok(frame_data)
