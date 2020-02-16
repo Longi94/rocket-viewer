aa7266fbe616db8b28439b26543914cb9530f61a
Long Tran <lngtrn94@gmail.com>
2020-02-16 18:33:42 +0100

    add default ball

--- src/app/scene/loader/ball.ts
+++ src/app/scene/loader/ball.ts
@@ -2,11 +2,17 @@ import { ReplayScene } from '../replay-scene';
 import { applyEnvMap } from '../../util/three';
 import { modelLoader } from './loader-config';
 
+const DEFAULT = '/assets/models/balls/ball_default.draco.glb';
+
 const BALL_MAPPING = {
-  'Basketball': '/assets/models/basketball.draco.glb'
+  'Basketball': '/assets/models/balls/ball_basketball.draco.glb'
 };
 
 export async function loadBall(type: string, rs: ReplayScene) {
-  rs.models.ball = (await modelLoader.load(BALL_MAPPING[type])).scene;
+  let url = BALL_MAPPING[type];
+  if (url == undefined) {
+    url = DEFAULT;
+  }
+  rs.models.ball = (await modelLoader.load(url)).scene;
   applyEnvMap(rs.models.ball, rs.envMap);
 }
