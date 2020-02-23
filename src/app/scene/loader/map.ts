23323260c2d04539639fa319b62b8b908fa82af9
Long Tran <lngtrn94@gmail.com>
2020-02-23 17:42:31 +0100

    update models

--- src/app/scene/loader/map.ts
+++ src/app/scene/loader/map.ts
@@ -10,7 +10,7 @@ const MAPPING = {
 export async function loadMap(name: string, rs: ReplayScene) {
   let mapUrl = MAPPING[name];
   if (mapUrl == undefined) {
-    mapUrl = '/assets/models/maps/TrainStation_P.draco.glb'
+    mapUrl = '/assets/models/maps/TrainStation_Night_P.draco.glb'
   }
   rs.models.map = (await modelLoader.load(mapUrl)).scene;
   applyEnvMap(rs.models.map, rs.envMap);
