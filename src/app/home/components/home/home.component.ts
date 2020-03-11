b258a60b6238f33042794947cac5dfd9201358b6
Long Tran <lngtrn94@gmail.com>
2020-03-12 00:18:42 +0100

    vr overlay

--- src/app/home/components/home/home.component.ts
+++ src/app/home/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BoxcarsService } from '../../../service/boxcars.service';
 import { PlaybackService } from '../../../service/playback.service';
+import { ThreeService } from '../../../service/three.service';
 
 @Component({
   selector: 'app-home',
@@ -13,9 +14,11 @@ export class HomeComponent implements OnInit {
   replayReady = false;
   dragging = false;
   errorMessage: string;
+  inVr = false;
 
   constructor(private readonly boxcarsService: BoxcarsService,
-              private readonly playbackService: PlaybackService) {
+              private readonly playbackService: PlaybackService,
+              private readonly threeService: ThreeService) {
     this.boxcarsService.onResult.subscribe(result => {
       if (typeof result === 'string') {
         this.errorMessage = result;
@@ -26,6 +29,8 @@ export class HomeComponent implements OnInit {
     this.playbackService.onPlaybackInfo.subscribe(() => {
       this.replayReady = true;
     });
+    this.threeService.onVrEnter.subscribe(() => this.inVr = true);
+    this.threeService.onVrLeave.subscribe(() => this.inVr = false);
   }
 
   ngOnInit(): void {
@@ -56,5 +61,9 @@ export class HomeComponent implements OnInit {
   onDragOver($event: DragEvent) {
     $event.stopPropagation();
     $event.preventDefault();
+  }
+
+  leaveVr() {
+    this.playbackService.leaveVr();
   }
 }
